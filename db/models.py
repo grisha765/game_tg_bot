1ed9569a4230eaf83443a798f8d8b5ae995746ca
Grisha <grisha765@tuta.io>
2024-07-08 23:37:26 +0300

    [u] add emoji model

--- db/models.py
+++ db/models.py
@@ -10,5 +10,15 @@ class Wins(Model):
         table = "wins"
         unique_together = ("user_id",)
 
+class EmojiPhrase(Model):
+    id = fields.IntField(pk=True)
+    chat_id = fields.IntField()
+    emoji = fields.CharField(max_length=10)
+    phrase = fields.CharField(max_length=255)
+
+    class Meta:
+        table = "EmojiPhrase"
+        unique_together = ("chat_id", "emoji")
+
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
