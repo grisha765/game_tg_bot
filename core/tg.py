4afa39bd0703f275f561809498fc334a3e00c657
Grisha <grisha765@tuta.io>
2024-07-08 22:11:55 +0300

    [+] add tg core

---
+++ core/tg.py
@@ -0,0 +1,39 @@
+from pyrogram import Client, filters
+from config.config import Config
+from casino.spin import spin_func
+from casino.point_check import check_wins, top_command
+from core.trans import get_translation
+from config import logging_config
+logging = logging_config.setup_logging(__name__)
+
+app = Client("bot", api_id=Config.tg_id, api_hash=Config.tg_hash, bot_token=Config.tg_token)
+
+@app.on_message(filters.command("help"))
+async def hanlde_help(_, message):
+    user_language = message.from_user.language_code
+    logging.debug(f"{message.from_user.id}: {user_language}")
+    text = get_translation(user_language, "help")
+    await message.reply_text(text)
+
+@app.on_message(filters.text & filters.command("spin", prefixes="/"))
+async def handle_spin(_, message):
+    await spin_func(message)
+
+@app.on_message(filters.text & filters.command("wins", prefixes="/"))
+async def handle_wins(_, message):
+    await check_wins(message)
+
+@app.on_message(filters.text & filters.command("top", prefixes="/"))
+async def handle_top(client, message):
+    await top_command(client, message)
+
+async def start_bot():
+    logging.info("Launching the bot...")
+    await app.start()
+
+async def stop_bot():
+    logging.info("Stopping the bot...")
+    await app.stop()
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")
