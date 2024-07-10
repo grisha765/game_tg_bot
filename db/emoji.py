4f1031dfe3a74d1e36502c5456cb0d8be5a8395a
Grisha <grisha765@tuta.io>
2024-07-10 18:35:52 +0300

    [u] add del emoji & [f] fix add emoji

--- db/emoji.py
+++ db/emoji.py
@@ -8,11 +8,24 @@ DEFAULT_PHRASES = [
     'БАКЛАЖАН! У вас ДЖЕКПОТ! Вставьте его поглубже...'
 ]
 
+last_replaced_index = {}
+
 async def add_emoji(chat_id: int, emoji: str, phrase: str):
-    emoji_phrase, created = await EmojiPhrase.get_or_create(chat_id=chat_id, emoji=emoji, defaults={'phrase': phrase})
-    if not created:
-        emoji_phrase.phrase = phrase
-        await emoji_phrase.save()
+    emoji_phrases = await EmojiPhrase.filter(chat_id=chat_id).order_by('id')
+
+    if len(emoji_phrases) < 4:
+        await EmojiPhrase.create(chat_id=chat_id, emoji=emoji, phrase=phrase)
+    else:
+        if chat_id not in last_replaced_index:
+            last_replaced_index[chat_id] = 0
+        
+        replace_index = last_replaced_index[chat_id]
+        emoji_phrases[replace_index].emoji = emoji
+        emoji_phrases[replace_index].phrase = phrase
+        await emoji_phrases[replace_index].save()
+
+        last_replaced_index[chat_id] = (replace_index + 1) % 4
+
     return {"status": "success", "data": {"chat_id": chat_id, "emoji": emoji, "phrase": phrase}}
 
 async def get_emoji(chat_id: int):
@@ -24,3 +37,7 @@ async def get_emoji(chat_id: int):
         return {"emoji": emojis, "phrases": phrases}
     
     return {"emoji": DEFAULT_EMOJIS, "phrases": DEFAULT_PHRASES}
+
+async def del_emoji(chat_id: int):
+    await EmojiPhrase.filter(chat_id=chat_id).delete()
+    return {"status": "success", "message": f"All emojis and phrases for chat_id {chat_id} have been deleted."}
