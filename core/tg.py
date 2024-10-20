1487a5ec1b1d57c091be3d3cccd3127844138d47
Grisha <grisha765@tuta.io>
2024-10-21 02:10:12 +0300

    [u] implementation in telegram

--- core/tg.py
+++ core/tg.py
@@ -6,7 +6,9 @@ from casino.point import check_wins, top_command
 from casino.emoji import set_emoji_command, get_emoji_command, del_emoji_command
 from core.trans import get_translation
 from chesse.invite import chess_start, join_chess_black, remove_expired_chess_session
+from tictactoe.invite import ttt_start, remove_expired_ttt_session, join_ttt_o
 from chesse.game import move_chess
+from tictactoe.game import move_ttt
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
@@ -74,6 +76,34 @@ async def handle_chess_move(client, callback_query):
     session_id, position = callback_query.data.split('_')
     session_id = int(session_id)
     selected_squares[session_id] = await move_chess(client, callback_query, sessions[session_id], selected_squares[session_id])
+
+@app.on_message(filters.text & filters.command("ttt", prefixes="/") & filters.group)
+async def handle_ttt_start(client, message):
+    if available_session_ids:
+        session_id = available_session_ids.pop(0)
+    else:
+        session_id = len(sessions) + 1
+    sessions[session_id] = {
+        "x": {"id": None, "name": None},
+        "o": {"id": None, "name": None},
+        "message_id": None,
+        "chat_id": message.chat.id
+    }
+    selected_squares[session_id] = None
+    sessions[session_id]["x"]["id"], sessions[session_id]["x"]["name"], message_id = await ttt_start(session_id, sessions, message)
+    sessions[session_id]["message_id"] = message_id
+    asyncio.create_task(remove_expired_ttt_session(session_id, sessions, selected_squares, available_session_ids, client))
+
+@app.on_callback_query(filters.regex(r"join_o_(\d+)"))
+async def handle_ttt_join(client, callback_query):
+    session_id = int(callback_query.data.split('_')[-1])
+    await join_ttt_o(session_id, sessions, client, callback_query)
+
+@app.on_callback_query(filters.regex(r"^(\d+)_(\d+)$"))
+async def handle_ttt_move(client, callback_query):
+    session_id, position = callback_query.data.split('_')
+    session_id = int(session_id)
+    await move_ttt(client, callback_query, sessions[session_id], int(position), session_id, sessions, selected_squares, available_session_ids)
 
 async def start_bot():
     logging.info("Launching the bot...")
